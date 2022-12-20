6c63c2929c15aee843a92388b433057cb134e7d3
gran-witgen <gran_dleon@yahoo.co.jp>
2022-12-21 02:49:06 +0900

    第一回目の課題

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("これは単発のHelloWorldです");
+        System.out.println("HelloWorld");
+
+        for (int i=1; i < 11; i++){
+            System.out.println("これは" + i + "回目のHelloWorldです");
+            System.out.println("HelloWorld");
+        }
     }
 }
