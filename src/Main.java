0894927f0ff03723aa976386474af3cafdf7dc84
gran-witgen <gran_dleon@yahoo.co.jp>
2022-12-22 18:14:20 +0900

    変数宣言時の型指定を変更

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,32 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
-        // ArrayList
-        ArrayList<String> names = new ArrayList<String>();
-        String [] students = {"tarou", "hanako", "kantoku", "poti"};
-        for (String student:  students){
+        //List
+        List<String> names = new ArrayList<String>();
+        String[] students = {"tarou", "hanako", "kantoku", "poti"};
+        for (String student : students) {
             names.add(student);
         }
         System.out.println(names);
         // 例外を発生、キャッチする
-        try{
+        try {
             System.out.println(names.get(4));
-        } catch (IndexOutOfBoundsException e){
+        } catch (IndexOutOfBoundsException e) {
             System.out.println(e);
         }
 
-        // HashMap
-        HashMap<String, Integer> people = new HashMap<String, Integer>();
+        //Map
+        Map<String, Integer> people = new HashMap<String, Integer>();
         people.put("agasa", 50);
         people.put("sasahara", 45);
         people.put("miura", 62);
 
-        for (String peopleNameKey: people.keySet()){
+        for (String peopleNameKey : people.keySet()) {
             System.out.println(peopleNameKey + "の年齢は" + people.get(peopleNameKey) + "歳です");
         }
     }
