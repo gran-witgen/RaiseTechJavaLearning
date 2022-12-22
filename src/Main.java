75a676ffad1a5571fa340e04ac417c977bd8f841
gran-witgen <gran_dleon@yahoo.co.jp>
2022-12-22 16:42:29 +0900

    第三回課題提出

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // ArrayList
+        ArrayList<String> names = new ArrayList<String>();
+        String [] students = {"tarou", "hanako", "kantoku", "poti"};
+        for (String student:  students){
+            names.add(student);
+        }
+        System.out.println(names);
+        // 例外を発生、キャッチする
+        try{
+            System.out.println(names.get(4));
+        } catch (IndexOutOfBoundsException e){
+            System.out.println(e);
+        }
+
+        // HashMap
+        HashMap<String, Integer> people = new HashMap<String, Integer>();
+        people.put("agasa", 50);
+        people.put("sasahara", 45);
+        people.put("miura", 62);
+
+        for (String peopleNameKey: people.keySet()){
+            System.out.println(peopleNameKey + "の年齢は" + people.get(peopleNameKey) + "歳です");
+        }
     }
 }
