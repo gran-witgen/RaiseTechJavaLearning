2e99f19b3f059c220293aef9def3aadbb5eaf799
gran-witgen <gran_dleon@yahoo.co.jp>
2022-12-23 16:49:21 +0900

    第四回課題提出

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        People people1 = new People("tarou");
+        People people2 = new People("hanako");
+        People people3 = new People("sakura");
+
+        // stream not use
+        List<People> peopleGroup1 = new ArrayList<People>(Arrays.asList(people1, people2, people3));
+        for (People e : peopleGroup1) {
+            System.out.println("名前は" + e.getName() + "です");
+        }
+
+        // stream use
+        List<People> peopleGroup2 = new ArrayList<People>(Arrays.asList(people1, people2, people3));
+        peopleGroup2.stream().forEach(p -> {
+            System.out.println("名前は" + p.getName() + "です");
+        });
     }
 }
---
+++ src/People.java
@@ -0,0 +1,11 @@
+public class People {
+    private String name;
+
+    public People(String name) {
+        this.name = name;
+    }
+
+    String getName() {
+        return this.name;
+    }
+}
